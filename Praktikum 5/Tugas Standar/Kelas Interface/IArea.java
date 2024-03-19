b5d1083788d4038a1c149874f373fb528fefa0aa
claraztn <160481864+claraztn@users.noreply.github.com>
2024-03-19 14:18:58 +0700

    Add files via upload

---
+++ Praktikum 5/Tugas Standar/Kelas Interface/IArea.java
@@ -0,0 +1,11 @@
+/** 
+ * Nama         : Clara Zita Nabilla
+ * NIM          : 24060122130057 
+ * File         : IArea.java
+ * Deskripsi    : Interface yang mendefinisikan kontrak bahwa setiap kelas yang membuat implementasi 
+ *                dari interface ini harus memiliki cara untuk menghitung luas
+ */
+
+public interface IArea {
+    public double hitungLuas();
+}
